73f8d038562153f906d08503e6c0f568d1ec4ef8
ViweTeko <iamviwe.teko@gmail.com>
2024-08-12 14:29:37 +0200

    Fixed syntax

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,7 +41,7 @@ class BasicAuth(Auth):
         if type(decoded_base64_authorization_header) == str:
             pattern = r'(?P<user>[^:]+):(?P<password>.+)'
             field_match = re.fullmatch(
-                pattern, decode_base64_authorization_header.strip(),
+                pattern, decoded_base64_authorization_header.strip(),
             )
             if field_match is not None:
                 user = field_match.group('user')
